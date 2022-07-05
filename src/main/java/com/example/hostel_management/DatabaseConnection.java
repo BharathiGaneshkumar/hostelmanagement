766c335ae66508df0ce4fe128c153b130116fb02
BharathiGaneshkumar <bharathi1010ad@gmail.com>
2022-07-05 12:23:00 +0530

    initial commit:committed

---
+++ src/main/java/com/example/hostel_management/DatabaseConnection.java
@@ -0,0 +1,18 @@
+package com.example.hostel_management;
+import java.sql.*;
+public class DatabaseConnection {
+       public Connection con;
+       public Connection getConnection() {
+           String dbname = "students";
+           String user = "root";
+           String password = "1b2h3a4r";
+           String url = "jdbc:mysql://localhost:3306/" + dbname;
+           try {
+               Class.forName("com.mysql.cj.jdbc.Driver");
+               con = DriverManager.getConnection(url, user, password);
+           } catch (Exception e) {
+               e.printStackTrace();
+           }
+           return con;
+       }
+}
